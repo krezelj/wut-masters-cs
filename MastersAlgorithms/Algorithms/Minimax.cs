ff07f6836769e32f7dcb5223c381a656aecefd15
Jasiek <jasiekk9@gmail.com>
2025-04-05 13:47:52 +0200

    Fix bugs in Minimax

--- MastersAlgorithms/Algorithms/Minimax.cs
+++ MastersAlgorithms/Algorithms/Minimax.cs
@@ -1,6 +1,4 @@
-#if DEBUG
 using System.Diagnostics;
-#endif
 
 using MastersAlgorithms.Games;
 
@@ -8,52 +6,46 @@ namespace MastersAlgorithms.Algorithms
 {
     public class Minimax<T>
     {
-#if DEBUG
+        private const int MAX_VAL = 10_000;
+
         private long _nodes = 0;
-#endif
         private int _depth;
 
-        private IGame<T> _game;
-        private T _bestMoveInRoot;
+        private IGame<T>? _game;
+        private T? _bestMoveInRoot;
 
         public Minimax(int depth)
         {
             _depth = depth;
         }
 
-        public T GetMove(IGame<T> game)
+        public T? GetMove(IGame<T> game)
         {
             _game = game;
-#if DEBUG
             var sw = new Stopwatch();
             sw.Start();
-#endif
-            Search(_depth, 0, int.MinValue, int.MaxValue);
-#if DEBUG
+            Search(_depth, 0, -MAX_VAL, MAX_VAL);
             Console.WriteLine($"Nodes: {_nodes} ({_nodes / (float)sw.ElapsedMilliseconds} kN/s)");
-#endif
             return _bestMoveInRoot;
         }
 
         private int Search(int depth, int ply, int alpha, int beta)
         {
-#if DEBUG
             _nodes++;
-#endif
             bool isRoot = ply == 0;
 
-            if (depth == 0 || _game.IsOver)
-                return _game.Evaluate();
+            if (depth == 0 || _game!.IsOver)
+                return _game!.Evaluate();
 
             var moves = _game.GetMoves();
             // TODO -- Order moves
 
-            int current_value = int.MinValue;
+            int current_value = -MAX_VAL;
             int new_value;
             foreach (var move in moves)
             {
                 _game.MakeMove(move);
-                new_value = Search(depth - 1, ply + 1, -beta, -alpha);
+                new_value = -Search(depth - 1, ply + 1, -beta, -alpha);
                 _game.UndoMove(move);
 
                 if (new_value > current_value)
@@ -62,8 +54,8 @@ namespace MastersAlgorithms.Algorithms
                     alpha = Math.Max(current_value, alpha);
                     if (isRoot)
                         _bestMoveInRoot = move;
-                    // if (alpha > beta)
-                    //     break; // prune
+                    if (alpha >= beta)
+                        break; // prune
 
                 }
             }
