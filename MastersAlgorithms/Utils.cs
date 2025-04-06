91804d3d325e48f253a7315f5e358edc43a4e6d4
Jasiek <jasiekk9@gmail.com>
2025-04-06 20:45:36 +0200

    Optimise GetNeighborDiffs

--- MastersAlgorithms/Utils.cs
+++ MastersAlgorithms/Utils.cs
@@ -20,9 +20,12 @@ namespace MastersAlgorithms
             return i >= 0 && i < width && j >= 0 && j < height;
         }
 
-        public static IEnumerable<(int di, int dj)> GetNeighborDiffs(
+        public static (int, int)[] GetNeighborDiffs(
             int i, int j, int width, int height)
         {
+            (int, int)[] diffs = new (int, int)[8];
+            int count = 0;
+
             for (int di = -1; di <= 1; di++)
             {
                 for (int dj = -1; dj <= 1; dj++)
@@ -34,20 +37,22 @@ namespace MastersAlgorithms
                     if (!InLimits(newI, newJ, width, height))
                         continue;
 
-                    yield return (di, dj);
+                    diffs[count++] = (di, dj);
                 }
             }
+            Array.Resize(ref diffs, count);
+            return diffs;
         }
 
-        public static IEnumerable<(int i, int j)> GetNeighbors(
-            int i, int j, int width, int height
-        )
-        {
-            var diffs = GetNeighborDiffs(i, j, width, height);
-            foreach (var diff in diffs)
-            {
-                yield return (i + diff.di, j + diff.dj);
-            }
-        }
+        // public static IEnumerable<(int i, int j)> GetNeighbors(
+        //     int i, int j, int width, int height
+        // )
+        // {
+        //     var diffs = GetNeighborDiffs(i, j, width, height);
+        //     foreach (var diff in diffs)
+        //     {
+        //         yield return (i + diff.di, j + diff.dj);
+        //     }
+        // }
     }
 }
